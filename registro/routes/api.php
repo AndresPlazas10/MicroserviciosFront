e6a6593f0e39f9bcc23d6c06fc1cd35a4b4bed54
AndresGit <afplazas@uniboyaca.edu.co>
2024-11-21 19:35:47 -0500

    Cambios de frontend

---
+++ registro/routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\estudianteController;
+use App\Http\Controllers\NotaController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::prefix("app")->group(function(){
+    Route::get('/estudiante', [EstudianteController::class, 'index']);
+
+    Route::get('/estudiante/{cod}', [EstudianteController::class, 'show']);
+    
+    Route::post('/estudiante', [EstudianteController::class, 'store']);
+    
+    Route::put('/estudiante/{cod}', [EstudianteController::class, 'update']);
+    
+    Route::delete('/estudiante/{cod}', [EstudianteController::class, 'destroy']);
+    
+    Route::get('/nota', [NotaController::class, 'index']);
+    
+    Route::post('/nota', [NotaController::class, 'store']);
+});
